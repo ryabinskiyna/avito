9c373bf8596cad774cce07be99b516542dd35d73
student@localhost <student@localhost>
2019-10-31 14:19:48 +0300

    Сreate application Сlasses, methods and database

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -10,6 +10,6 @@ public class Main {
         System.out.println(service.sortBy((o1, o2) -> o2.getPrice() - o1.getPrice()));
         System.out.println(service.sortBy((o1, o2) -> o1.getDistrict().compareToIgnoreCase(o2.getDistrict())));
         System.out.println(service.searchByUnderground("сев", (o1, o2) -> o1.getUnderground().compareToIgnoreCase(o2.getUnderground())));
-        System.out.println(service.searchByPrice(2000000, 4000000, (o1, o2) -> o1.getPrice() - o2.getPrice()));
+        System.out.println(service.searchByPrice(2_000_000, 4_000_000, (o1, o2) -> o1.getPrice() - o2.getPrice()));
     }
 }
