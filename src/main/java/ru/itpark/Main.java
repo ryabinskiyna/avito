976b8c40ba392d8f1b71bf4cadbfbf34460230c0
student@localhost <student@localhost>
2019-11-15 20:36:09 +0300

    Transfer Avito entirely to JdbcTemplate, provide the ability to replace the repository implementation with JDBC and Inmemory

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -7,20 +7,20 @@ import ru.itpark.service.HouseService;
 
 public class Main {
     public static void main(String[] args) {
-        final HouseService service = new HouseService(new HouseRepositoryJdbcImpl("jdbc:sqlite:db.sqlite"));
-        service.register(new House(0, 10000000, "Вахитовский", "Площадь Тукая"));
-        service.renew(new House(16, 1, "а", "б"));
-        System.out.println(service.searchByPrice(2000000, 3000000, (o1, o2) -> o1.getPrice() - o2.getPrice()));
-        System.out.println(service.searchByDistrict("Вахитовский", (o1, o2) -> o1.getPrice() - o2.getPrice()));
-        System.out.println(service.sortBy((o1, o2) -> o2.getPrice() - o1.getPrice()));
+        final HouseService serviceJdbc = new HouseService(new HouseRepositoryJdbcImpl("jdbc:sqlite:db.sqlite"));
+        serviceJdbc.register(new House(0, 10000000, "Вахитовский", "Площадь Тукая"));
+        serviceJdbc.renew(new House(16, 1, "а", "б"));
+        System.out.println(serviceJdbc.searchByPrice(2000000, 3000000, (o1, o2) -> o1.getPrice() - o2.getPrice()));
+        System.out.println(serviceJdbc.searchByDistrict("Вахитовский", (o1, o2) -> o1.getPrice() - o2.getPrice()));
+        System.out.println(serviceJdbc.sortBy((o1, o2) -> o2.getPrice() - o1.getPrice()));
 
-        HouseService service1 = new HouseService(new HouseRepositoryInMemoryImpl());
-        service1.register(new House(0, 1, "а", "б"));
-        service1.register(new House(0, 2, "в", "г"));
-        service1.register(new House(0, 3, "д", "е"));
-        service1.renew(new House(3, 300, "д", "е"));
+        HouseService serviceInMemory = new HouseService(new HouseRepositoryInMemoryImpl());
+        serviceInMemory.register(new House(0, 1, "а", "б"));
+        serviceInMemory.register(new House(0, 2, "в", "г"));
+        serviceInMemory.register(new House(0, 3, "д", "е"));
+        serviceInMemory.renew(new House(3, 300, "д", "е"));
 
-        System.out.println(service1.sortBy((o1, o2) -> o1.getPrice() - o2.getPrice()));
+        System.out.println(serviceInMemory.sortBy((o1, o2) -> o1.getPrice() - o2.getPrice()));
 
 
     }
